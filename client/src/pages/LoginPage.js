f2e78d9d1129bc981133fb23f33d37cefee881a0
Kevin Spies <kevin.spies@gmail.com>
2019-08-23 08:31:50 -0400

    adding import and route to home page (after login), throwing some component in for home page, giving login access to API, and making a create user post request therein

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import API from "../utils/API";
 
 class LoginPage extends Component {
   state = {
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default {
   //todo
+  createUser: function(userData) {
+    return axios.post("api/users", userData);
+  }
 };

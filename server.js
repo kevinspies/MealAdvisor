08f4b858c0eb20254259db385da58b7c89d562fa
Kevin Spies <kevin.spies@gmail.com>
2019-08-23 09:34:11 -0400

    requiring and installing mongoose at root, telling express app to use routes, also i'm getting callback errors, which is oddly okay because that means at leaszt the routes are attempting something, right now i have some template book route code so i expected that

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
 // Define API routes here
 
 mongoose.connect(

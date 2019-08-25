348ea145cade63d5723ed73feb50ae66c41cedcf
Kevin Spies <kevin.spies@gmail.com>
2019-08-25 08:20:31 -0400

    setting up more pages, making navs better, making the site somewhat demoable from just a view standpoint, next I'll just add some function into home to try to figure out my menu rolling method

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import EditPage from "./pages/EditPage";
-import Nav from "./components/Nav";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route exact path="/" component={LoginPage} />
           <Route exact path="/users/" component={HomePage} />
           <Route exact path="/edit/" component={EditPage} />
+          <Route exact path="/about/" component={AboutPage} />
         </Switch>
       </div>
     </Router>
--- client/src/pages/EditPage.js
+++ client/src/pages/EditPage.js
@@ -3,6 +3,7 @@ import API from "../utils/API";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import { Col, Row, Container } from "../components/Grid";
 import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 class EditPage extends Component {
   state = {
@@ -20,7 +21,7 @@ class EditPage extends Component {
   render() {
     return (
       <div>
-        //la la la
+        <Nav />
         <h1>edit your body's stats!</h1>
         <Input value={this.state.name} placeholder="name (required)" />
         <Input value={this.state.age} placeholder="age (required)" />

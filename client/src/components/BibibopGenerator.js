05280b2a3129e4bd17c73ba993bc78a3501aa9b1
Kevin Spies <kevin.spies@gmail.com>
2019-08-25 09:21:49 -0400

    starting to work on a functional mealGenerator which I will hopefully be able to turn into a rendering component to have something cool to show

---
+++ client/src/components/BibibopGenerator.js
@@ -0,0 +1,27 @@
+const menu = {
+  base: [purpleRice, japchaeNoodles],
+  protein: [spicyChicken, sweetChicken, Beef],
+  hotToppings: [beanSprouts, blackBeans, potatoes],
+  coldToppings: [
+    carrots,
+    cheese,
+    corn,
+    cucumber,
+    eggs,
+    daikon,
+    kale,
+    pickledRedCabbage,
+    redCabbage
+  ],
+  sauce: [
+    yumYum,
+    teriaki,
+    gochujang,
+    cucumberWasabi,
+    spicySriracha,
+    sesameGinger
+  ],
+  sides: [kimchi, misoSoup]
+};
+
+function generateBibiMeal(caloriesRemaining, bibiMenuObject) {}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,11 +8,20 @@ import { Link } from "react-router-dom";
 
 class HomePage extends Component {
   state = {
-    //blah
+    remainingCalories: 1900
   };
 
   loadUser = id => {
     //load users name, weight
+    //on page load
+  };
+
+  //takes in the remaining number of calories needed for that day
+  generateBibibopMeal = function(calories) {
+    //i want this to return a json object representing one potential meal off the menu
+    //i can just define the bibi menu right here to start out
+    //rought plan, select one base, see where i'm at, one protein, see where i'm at, one topping, see where I'm at, (check) one dressing, check, then
+    //one more topping, check, one more topping, check, and then start randomizing betwwen more toppings and sides, something like this
   };
 
   render() {
@@ -24,7 +33,9 @@ class HomePage extends Component {
           <Jumbotron>You Got This!</Jumbotron>
         </Row>
         <Row>
-          <Col size="md-6 md-offset-1">Your Brunch:</Col>
+          <Col size="md-6 md-offset-1">
+            Your Brunch:{this.generateBibibopMeal}
+          </Col>
           <Col size="md-6 md-offset-1">Your Dinner:</Col>
         </Row>
       </Container>
